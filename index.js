61d2eb946f9055b9d47d263489ab3ce658da7fcd
huan9huan <xuan@xuan-mac.lan>
2017-01-26 10:08:45 +0800

    separate the exports botd class

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
 'use strict';
 
-var program = require('commander');
-var co = require('co');
-var logger = require('./logger');
-var SlackBot = require('./slack');
-var Connector = require('./connector');
-
-function checkUsage(token) {
-    if(!token)
-        throw new Error("bad input, need -t <token>");
-}
-
-program
-    .option('-t, --token <token>', 'specify the token', '')
-    .option('-n, --name <name>', 'specify the bot running name', '')
-    .parse(process.argv);
-checkUsage(program.token, program.name);
-
-var slack = new SlackBot(program.token, program.name);
-var connector = new Connector(slack);
-slack.connect(connector);
-slack.startBot();
-
+exports.logger = require('./logger');
+exports.SlackBot = require('./slack');
+exports.Connector = require('./connector');
+exports.Apps = require('./apps').common;
