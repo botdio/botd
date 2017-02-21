6c98a21eda7418d344dd613ad84c6cd95fdc4d57
huan9huan <xuan@xuan-mac.local>
2017-02-21 09:57:48 +0800

    fix bugs

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ exports.SlackBot = require('./slack');
 exports.Connector = require('./connector');
 exports.Apps = require('./apps').common;
 
-exports.Bash = require('./Bash');
-exports.Node = require('./Node');
+exports.Bash = require('./bash');
+exports.Node = require('./bode');
 exports.Console = require('./utils/console');
